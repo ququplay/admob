f14eb4b3b1c9747862e2fbb6bef9bd0ed4e0ac45
Michal Kuklis <michal.kuklis@gmail.com>
2024-01-10 10:55:30 -0500

    Add canShowPersonalizedAds and canShowAds to consent respones

--- ios/Sources/AdMobPlugin/Consent/ConsentExecutor.swift
+++ ios/Sources/AdMobPlugin/Consent/ConsentExecutor.swift
@@ -28,6 +28,8 @@ class ConsentExecutor: NSObject {
                         "isConsentFormAvailable": ConsentInformation.shared.formStatus == FormStatus.available,
                         "canRequestAds": ConsentInformation.shared.canRequestAds,
                         "privacyOptionsRequirementStatus": self.getPrivacyOptionsRequirementStatus(ConsentInformation.shared.privacyOptionsRequirementStatus)
+                        "canShowAds": self.canShowAds(),
+                        "canShowPersonalizedAds": self.canShowPersonalizedAds()
                     ])
                 }
             })
@@ -56,13 +58,37 @@ class ConsentExecutor: NSObject {
             if formStatus == FormStatus.available {
                 Task { @MainActor in
                     do {
-                        try await ConsentForm.loadAndPresentIfRequired(from: rootViewController)
+                        ConsentForm.load(completionHandler: {form, loadError in
+                            if loadError != nil {
+                                call.reject(loadError?.localizedDescription ?? "Load consent form error")
+                                return
+                            }
 
-                        call.resolve([
-                            "status": self.getConsentStatusString(ConsentInformation.shared.consentStatus),
-                            "canRequestAds": ConsentInformation.shared.canRequestAds,
-                            "privacyOptionsRequirementStatus": self.getPrivacyOptionsRequirementStatus(ConsentInformation.shared.privacyOptionsRequirementStatus)
-                        ])
+                            if ConsentInformation.shared.consentStatus == ConsentStatus.required {
+                                form?.present(from: rootViewController, completionHandler: { dismissError in
+                                    if dismissError != nil {
+                                        call.reject(dismissError?.localizedDescription ?? "Consent dismiss error")
+                                        return
+                                    }
+
+                                    call.resolve([
+                                        "status": self.getConsentStatusString(ConsentInformation.shared.consentStatus),
+                                        "canRequestAds": ConsentInformation.shared.canRequestAds,
+                                        "privacyOptionsRequirementStatus": self.getPrivacyOptionsRequirementStatus(ConsentInformation.shared.privacyOptionsRequirementStatus),
+                                        "canShowAds": self.canShowAds(),
+                                        "canShowPersonalizedAds": self.canShowPersonalizedAds()
+                                    ])
+                                })
+                            } else {
+                                call.resolve([
+                                    "status": self.getConsentStatusString(ConsentInformation.shared.consentStatus),
+                                    "canRequestAds": ConsentInformation.shared.canRequestAds,
+                                    "privacyOptionsRequirementStatus": self.getPrivacyOptionsRequirementStatus(ConsentInformation.shared.privacyOptionsRequirementStatus),
+                                    "canShowAds": self.canShowAds(),
+                                    "canShowPersonalizedAds": self.canShowPersonalizedAds()
+                                ])
+                            }
+                        })
                     } catch {
                         call.reject("Request consent info failed")
                     }
@@ -75,10 +101,12 @@ class ConsentExecutor: NSObject {
         }
     }
 
-    func resetConsentInfo(_ call: CAPPluginCall) {
-        ConsentInformation.shared.reset()
-        call.resolve()
+    public void resetConsentInfo(final PluginCall call, BiConsumer<String, JSObject> notifyListenersFunction) {
+        ensureConsentInfo();
+        consentInformation.reset();
+        call.resolve();
     }
+
 
     func getConsentStatusString(_ consentStatus: ConsentStatus) -> String {
         switch consentStatus {
@@ -102,5 +130,70 @@ class ConsentExecutor: NSObject {
         default:
             return "UNKNOWN"
         }
+    }
+
+    func isGDPR() -> Bool {
+        let settings = UserDefaults.standard
+        let gdpr = settings.integer(forKey: "IABTCF_gdprApplies")
+        return gdpr == 1
+    }
+
+    // Check if a binary string has a "1" at position "index" (1-based)
+    private func hasAttribute(input: String, index: Int) -> Bool {
+        return input.count >= index && String(Array(input)[index-1]) == "1"
+    }
+
+    // Check if consent is given for a list of purposes
+    private func hasConsentFor(_ purposes: [Int], _ purposeConsent: String, _ hasVendorConsent: Bool) -> Bool {
+        return purposes.allSatisfy { i in hasAttribute(input: purposeConsent, index: i) } && hasVendorConsent
+    }
+
+    // Check if a vendor either has consent or legitimate interest for a list of purposes
+    private func hasConsentOrLegitimateInterestFor(_ purposes: [Int], _ purposeConsent: String, _ purposeLI: String, _ hasVendorConsent: Bool, _ hasVendorLI: Bool) -> Bool {
+        return purposes.allSatisfy { i in
+            (hasAttribute(input: purposeLI, index: i) && hasVendorLI) ||
+            (hasAttribute(input: purposeConsent, index: i) && hasVendorConsent)
+        }
+    }
+
+    private func canShowAds() -> Bool {
+        let settings = UserDefaults.standard
+
+        //https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/TCFv2/IAB%20Tech%20Lab%20-%20CMP%20API%20v2.md#in-app-details
+        //https://support.google.com/admob/answer/9760862?hl=en&ref_topic=9756841
+
+        let purposeConsent = settings.string(forKey: "IABTCF_PurposeConsents") ?? ""
+        let vendorConsent = settings.string(forKey: "IABTCF_VendorConsents") ?? ""
+        let vendorLI = settings.string(forKey: "IABTCF_VendorLegitimateInterests") ?? ""
+        let purposeLI = settings.string(forKey: "IABTCF_PurposeLegitimateInterests") ?? ""
+
+        let googleId = 755
+        let hasGoogleVendorConsent = hasAttribute(input: vendorConsent, index: googleId)
+        let hasGoogleVendorLI = hasAttribute(input: vendorLI, index: googleId)
+
+        // Minimum required for at least non-personalized ads
+        return hasConsentFor([1], purposeConsent, hasGoogleVendorConsent)
+            && hasConsentOrLegitimateInterestFor([2,7,9,10], purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI)
+
+    }
+
+    private func canShowPersonalizedAds() -> Bool {
+        let settings = UserDefaults.standard
+
+        //https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/TCFv2/IAB%20Tech%20Lab%20-%20CMP%20API%20v2.md#in-app-details
+        //https://support.google.com/admob/answer/9760862?hl=en&ref_topic=9756841
+
+        // required for personalized ads
+        let purposeConsent = settings.string(forKey: "IABTCF_PurposeConsents") ?? ""
+        let vendorConsent = settings.string(forKey: "IABTCF_VendorConsents") ?? ""
+        let vendorLI = settings.string(forKey: "IABTCF_VendorLegitimateInterests") ?? ""
+        let purposeLI = settings.string(forKey: "IABTCF_PurposeLegitimateInterests") ?? ""
+
+        let googleId = 755
+        let hasGoogleVendorConsent = hasAttribute(input: vendorConsent, index: googleId)
+        let hasGoogleVendorLI = hasAttribute(input: vendorLI, index: googleId)
+
+        return hasConsentFor([1,3,4], purposeConsent, hasGoogleVendorConsent)
+            && hasConsentOrLegitimateInterestFor([2,7,9,10], purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI)
     }
 }
