0f92bcb1937cf29cb87eef2687edb969d847992c
Michal Kuklis <michal.kuklis@gmail.com>
2024-04-26 09:42:51 -0400

    Cleanup

--- android/src/main/java/com/getcapacitor/community/admob/consent/AdConsentExecutor.java
+++ android/src/main/java/com/getcapacitor/community/admob/consent/AdConsentExecutor.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-
 import androidx.core.util.Supplier;
 import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
@@ -17,7 +16,6 @@ import com.google.android.ump.ConsentDebugSettings;
 import com.google.android.ump.ConsentInformation;
 import com.google.android.ump.ConsentRequestParameters;
 import com.google.android.ump.UserMessagingPlatform;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,13 +32,13 @@ public class AdConsentExecutor extends Executor {
         super(contextSupplier, activitySupplier, notifyListenersFunction, pluginLogTag, "AdConsentExecutor");
     }
 
-    public boolean canShowPersonalizedAds(){
+    public boolean canShowPersonalizedAds() {
         Context context = contextSupplier.get();
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
         String purposeConsent = prefs.getString("IABTCF_PurposeConsents", "");
-        String vendorConsent = prefs.getString("IABTCF_VendorConsents","");
-        String vendorLI = prefs.getString("IABTCF_VendorLegitimateInterests","");
-        String purposeLI = prefs.getString("IABTCF_PurposeLegitimateInterests","");
+        String vendorConsent = prefs.getString("IABTCF_VendorConsents", "");
+        String vendorLI = prefs.getString("IABTCF_VendorLegitimateInterests", "");
+        String purposeLI = prefs.getString("IABTCF_PurposeLegitimateInterests", "");
 
         int googleId = 755;
         boolean hasGoogleVendorConsent = hasAttribute(vendorConsent, googleId);
@@ -57,17 +55,19 @@ public class AdConsentExecutor extends Executor {
         indexesLI.add(9);
         indexesLI.add(10);
 
-        return hasConsentFor(indexes, purposeConsent, hasGoogleVendorConsent)
-                && hasConsentOrLegitimateInterestFor(indexesLI, purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI);
-
+        return (
+            hasConsentFor(indexes, purposeConsent, hasGoogleVendorConsent) &&
+            hasConsentOrLegitimateInterestFor(indexesLI, purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI)
+        );
     }
-    public boolean canShowAds(){
+
+    public boolean canShowAds() {
         Context context = contextSupplier.get();
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
         String purposeConsent = prefs.getString("IABTCF_PurposeConsents", "");
-        String vendorConsent = prefs.getString("IABTCF_VendorConsents","");
-        String vendorLI = prefs.getString("IABTCF_VendorLegitimateInterests","");
-        String purposeLI = prefs.getString("IABTCF_PurposeLegitimateInterests","");
+        String vendorConsent = prefs.getString("IABTCF_VendorConsents", "");
+        String vendorLI = prefs.getString("IABTCF_VendorLegitimateInterests", "");
+        String purposeLI = prefs.getString("IABTCF_PurposeLegitimateInterests", "");
 
         int googleId = 755;
         boolean hasGoogleVendorConsent = hasAttribute(vendorConsent, googleId);
@@ -82,9 +82,10 @@ public class AdConsentExecutor extends Executor {
         indexesLI.add(9);
         indexesLI.add(10);
 
-        return hasConsentFor(indexes, purposeConsent, hasGoogleVendorConsent)
-                && hasConsentOrLegitimateInterestFor(indexesLI, purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI);
-
+        return (
+            hasConsentFor(indexes, purposeConsent, hasGoogleVendorConsent) &&
+            hasConsentOrLegitimateInterestFor(indexesLI, purposeConsent, purposeLI, hasGoogleVendorConsent, hasGoogleVendorLI)
+        );
     }
 
     @PluginMethod
@@ -230,11 +231,11 @@ public class AdConsentExecutor extends Executor {
 
     private boolean hasAttribute(String input, int index) {
         if (input == null) return false;
-        return input.length() >= index && input.charAt(index-1) == '1';
+        return input.length() >= index && input.charAt(index - 1) == '1';
     }
 
     private boolean hasConsentFor(List<Integer> indexes, String purposeConsent, boolean hasVendorConsent) {
-        for (Integer p: indexes) {
+        for (Integer p : indexes) {
             if (!hasAttribute(purposeConsent, p)) {
                 return false;
             }
@@ -242,12 +243,18 @@ public class AdConsentExecutor extends Executor {
         return hasVendorConsent;
     }
 
-    private boolean hasConsentOrLegitimateInterestFor(List<Integer> indexes, String purposeConsent, String purposeLI, boolean hasVendorConsent, boolean hasVendorLI){
-        for (Integer p: indexes) {
+    private boolean hasConsentOrLegitimateInterestFor(
+        List<Integer> indexes,
+        String purposeConsent,
+        String purposeLI,
+        boolean hasVendorConsent,
+        boolean hasVendorLI
+    ) {
+        for (Integer p : indexes) {
             boolean purposeAndVendorLI = hasAttribute(purposeLI, p) && hasVendorLI;
             boolean purposeConsentAndVendorConsent = hasAttribute(purposeConsent, p) && hasVendorConsent;
             boolean isOk = purposeAndVendorLI || purposeConsentAndVendorConsent;
-            if (!isOk){
+            if (!isOk) {
                 return false;
             }
         }
